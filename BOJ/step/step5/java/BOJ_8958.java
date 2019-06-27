42e40774f3e632e053b400f08c62b433af5977da
metalrose.hong <metalrose.hong@gmail.com>
2019-06-27 21:42:27 +0900

    BOJ step5 8958 changed

--- BOJ/step/step5/java/BOJ_8958.java
+++ BOJ/step/step5/java/BOJ_8958.java
@@ -2,7 +2,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class Main {
+public class BOJ_8958 {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
